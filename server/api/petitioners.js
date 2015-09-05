2bac0c2c7eaa65b028be9f09c38cb615512a9d43
Balakrishnan (Balki) Ranganathan <balki@live.com>
2015-09-05 11:54:50 -0700

    Allow logic

--- server/api/petitioners.js
+++ server/api/petitioners.js
@@ -1,50 +1,38 @@
 var mongo = require('mongodb'),
-	nconf = require('nconf'),
-	assert = require('assert');
+	nconf = require('nconf');
 
-var Server = mongo.Server,
-	Db = mongo.Db,
-	Client = mongo.MongoClient,
+var Client = mongo.MongoClient,
 	ObjectId = mongo.ObjectID;
 
 nconf.env(['mongodb:connection'])
 	.file('api/config.json');
 
 var uri = nconf.get('mongodb:connection');
-var os = require('os');
-var networkInterfaces = os.networkInterfaces();
-console.log(os.hostname());
-console.log(networkInterfaces);
+var allow = !nconf.get('RunningOnIISNode');
 
-var Allowed = function (req, res) {
-	//var ip = req.ip || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
-	//console.log(ip);
-	//return (ip === '');
-	
-	console.log(req.hostname);
-	console.dir(req.ips);
-
-	return true;
+var Allowed = function (req) {
+	return allow || req.hostname === 'greymind-dev.azurewebsites.net';
 }
 
 module.exports.GetAll = function (req, res) {
-	if (!Allowed(req, res)) {
+	if (!Allowed(req)) {
 		res.end(403, 'Forbidden');
 	};
 
 	Client.connect(uri, function (err, db) {
-		assert.equal(err, null);
-
 		db.collection('WeWantTrubel-Petitioners')
 			.find({}, { "Name": 1, "Location": 1, "TimeStamp": 1 })
 			.sort({ "TimeStamp": -1 })
 			.toArray(function (err, docs) {
-				assert.equal(err, null);
 				res.send(docs);
 			});
 	});
 };
 
 module.exports.Post = function (req, res) {
+	if (!Allowed(req)) {
+		res.end(403, 'Forbidden');
+	};
+
 
 };
