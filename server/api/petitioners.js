e586ae98ba35a7cbd1319a3352c765c24c2b0d34
Balakrishnan (Balki) Ranganathan <balki@live.com>
2015-09-05 11:30:41 -0700

    API test on Azure

--- server/api/petitioners.js
+++ server/api/petitioners.js
@@ -11,17 +11,17 @@ nconf.env(['mongodb:connection'])
 	.file('api/config.json');
 
 var uri = nconf.get('mongodb:connection');
+var os = require('os');
+var networkInterfaces = os.networkInterfaces();
+console.log(os.hostname());
+console.log(networkInterfaces);
 
-var os = require( 'os' );
-
-var networkInterfaces = os.networkInterfaces( );
-
-console.log( networkInterfaces );
-
-var Allowed = function (req) {
-	var ip = req.ip || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
-	console.log(ip);
-	return (ip === '');
+var Allowed = function (req, res) {
+	//var ip = req.ip || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
+	//console.log(ip);
+	//return (ip === '');
+	
+	res.send(req);
 }
 
 module.exports.GetAll = function (req, res) {
