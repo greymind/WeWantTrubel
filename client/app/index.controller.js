c785d295733ba4090caa1d14397528f3aa67ffbd
Balakrishnan (Balki) Ranganathan <balki@live.com>
2015-09-06 13:48:45 -0700

    Added level and progress bar

--- client/app/index.controller.js
+++ client/app/index.controller.js
@@ -12,9 +12,28 @@
 
         $http.get('/api/petitioners')
             .success(function (data) {
-                $scope.Petitioners = data;
+                var petitioners = data;
+                var petitionersCount = petitioners.length;
+                
+                var level = Math.max((Math.log(petitionersCount / 100.0) / (Math.log(2))), 0) + 1;
+                var levelCeiling = Math.ceil(level);
+                level = levelCeiling;
+
+                var levelMax = Math.pow(2, levelCeiling - 1) * 100;
+
+                if (petitionersCount == levelMax) {
+                    ++level;
+                    levelMax = Math.pow(2, levelCeiling - 1) * 100;
+                }
+
+                var progressPercentage = sprintf("%d%%", petitionersCount / levelMax * 100);
+                
+                $scope.Petitioners = petitioners;
+                $scope.Level = sprintf("%'02d", level);
+                $scope.LevelMax = levelMax;
+                $scope.ProgressPercentage = progressPercentage;
             });
-            
+
         $scope.FormatTimeStamp = function (timeStamp) {
             return moment(timeStamp).format("MMM DD, HH:mm");
         }
