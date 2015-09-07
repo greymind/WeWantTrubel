db46ca7f9ce7016cc20108d84556c21efe1228d6
Balakrishnan (Balki) Ranganathan <balki@live.com>
2015-09-07 06:14:51 -0700

    Added messaing support to refresh list of petitioners

--- client/app/index.controller.js
+++ client/app/index.controller.js
@@ -10,32 +10,40 @@
     function IndexController($scope, $http) {
         $scope.Title = '#WeWantTrubel';
 
-        $http.get('/api/petitioners')
-            .success(function (data) {
-                var petitioners = data;
-                var petitionersCount = petitioners.length;
-                
-                var level = Math.max((Math.log(petitionersCount / 100.0) / (Math.log(2))), 0) + 1;
-                var levelCeiling = Math.ceil(level);
-                level = levelCeiling;
+        var GetAllPetitioners = function () {
+            $http.get('/api/petitioners')
+                .success(function (data) {
+                    var petitioners = data;
+                    var petitionersCount = petitioners.length;
 
-                var levelMax = Math.pow(2, levelCeiling - 1) * 100;
+                    var level = Math.max((Math.log(petitionersCount / 100.0) / (Math.log(2))), 0) + 1;
+                    var levelCeiling = Math.ceil(level);
+                    level = levelCeiling;
 
-                if (petitionersCount == levelMax) {
-                    ++level;
-                    levelMax = Math.pow(2, levelCeiling - 1) * 100;
-                }
+                    var levelMax = Math.pow(2, levelCeiling - 1) * 100;
 
-                var progressPercentage = sprintf("%d%%", petitionersCount / levelMax * 100);
-                
-                $scope.Petitioners = petitioners;
-                $scope.Level = sprintf("%'02d", level);
-                $scope.LevelMax = levelMax;
-                $scope.ProgressPercentage = progressPercentage;
-            });
+                    if (petitionersCount == levelMax) {
+                        ++level;
+                        levelMax = Math.pow(2, levelCeiling - 1) * 100;
+                    }
+
+                    var progressPercentage = sprintf("%d%%", petitionersCount / levelMax * 100);
+
+                    $scope.Petitioners = petitioners;
+                    $scope.Level = sprintf("%'02d", level);
+                    $scope.LevelMax = levelMax;
+                    $scope.ProgressPercentage = progressPercentage;
+                });
+        };
+
+        $scope.$on('GetAllPetitioners', function (args) {
+            GetAllPetitioners();
+        });
 
         $scope.FormatTimeStamp = function (timeStamp) {
             return moment(timeStamp).format("MMM DD, HH:mm");
         }
+        
+        GetAllPetitioners();
     }
 })();
