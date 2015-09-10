2dccea9af24da649be4960bfe4ae7d2aa0734dda
Balakrishnan (Balki) Ranganathan <balki@live.com>
2015-09-10 09:54:23 -0700

    Argh! Should've added bower install first up!

--- deploy.cmd
+++ deploy.cmd
@@ -105,7 +105,15 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   popd
 )
 
-:: 4. Run gulp transformations
+:: 4. Install bower packages
+IF EXIST "%DEPLOYMENT_TARGET%\bower.json" (
+  pushd "%DEPLOYMENT_TARGET%"
+  call :ExecuteCmd .\node_modules\.bin\bower install
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
+)
+
+:: 5. Run gulp transformations
 IF EXIST "%DEPLOYMENT_TARGET%\gulpfile.js" (
   pushd "%DEPLOYMENT_TARGET%"
   call :ExecuteCmd .\node_modules\.bin\gulp
